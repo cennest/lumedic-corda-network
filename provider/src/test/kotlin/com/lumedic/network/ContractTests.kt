144fe01825cb26e2ee9a4a5effe768c7479521d4
Moorthy Yadav <moorthy.yadav@cennest.com>
2019-03-14 15:25:00 +0530

    Commented out provider testcases

--- provider/src/test/kotlin/com/lumedic/network/ContractTests.kt
+++ provider/src/test/kotlin/com/lumedic/network/ContractTests.kt
@@ -1,34 +1,34 @@
-package com.lumedic.network
-
-import com.lumedic.network.entity.*
-import io.vertx.core.Vertx
-import net.corda.testing.node.MockServices
-import org.junit.Test
-
-class ContractTests {
-    private val ledgerServices = MockServices()
-
-    @Test
-    fun `dummy test`() {
-
-    }
-
-    @Test
-    fun `report test`() {
-
-        /*
-        val vertx = Vertx.vertx()
-
-        val sim = SimulatorStateRefresher(vertx)
-        val sub = object : SimulatorStateSubscriber(vertx, ReportType.MTD, "PSJH" ){
-            override fun onNotify(update: AnalysisReport) {
-                val a = update
-            }
-        }
-
-        while (true){
-            Thread.sleep(2000)
-        }
-*/
-    }
-}
+//package com.lumedic.network
+//
+//import com.lumedic.network.entity.*
+//import io.vertx.core.Vertx
+//import net.corda.testing.node.MockServices
+//import org.junit.Test
+//
+//class ContractTests {
+//    private val ledgerServices = MockServices()
+//
+//    @Test
+//    fun `dummy test`() {
+//
+//    }
+//
+//    @Test
+//    fun `report test`() {
+//
+//        /*
+//        val vertx = Vertx.vertx()
+//
+//        val sim = SimulatorStateRefresher(vertx)
+//        val sub = object : SimulatorStateSubscriber(vertx, ReportType.MTD, "PSJH" ){
+//            override fun onNotify(update: AnalysisReport) {
+//                val a = update
+//            }
+//        }
+//
+//        while (true){
+//            Thread.sleep(2000)
+//        }
+//*/
+//    }
+//}
