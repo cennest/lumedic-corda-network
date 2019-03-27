739f401bc0ca99ed6622fd5ab387ce107601cfd9
Moorthy Yadav <moorthy.yadav@cennest.com>
2019-03-27 16:38:44 +0530

    testing

--- payer/src/main/kotlin/com/lumedic/network/payer/Flows.kt
+++ payer/src/main/kotlin/com/lumedic/network/payer/Flows.kt
@@ -44,13 +44,9 @@ object ProposalFlow {
 
     @InitiatedBy(GetSignedTransaction::class)
     class Acceptor(val session: FlowSession) :FlowLogic<Unit>() {
-
-
         @Suspendable
         override fun call() {
             val request = session.receive<SignedTransaction>().unwrap { it }
-
-
             val key = this.serviceHub.myInfo.legalIdentities.first().owningKey
             val sign = this.serviceHub.createSignature(request, key)
             val signed = request.withAdditionalSignature(sign)
